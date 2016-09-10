709b0d61fccac2840e304778b4cc17da562115c0
Diego Giacomelli <giacomelli@gmail.com>
2016-09-10 15:37:30 -0300

    Fixing project path.

--- tools/build-ci.cmd
+++ tools/build-ci.cmd
@@ -1,12 +1,12 @@
 echo Building jumpstart.exe...
 
-mkdir C:\projects\Giacomelli-Jumpstart\build
-cd C:\projects\Giacomelli-Jumpstart\src\Giacomelli-Jumpstart\bin\Release
+mkdir C:\projects\jumpstart\build
+cd C:\projects\jumpstart\src\Giacomelli-Jumpstart\bin\Release
 
 echo Calling ILRepack...
-C:\projects\Giacomelli-Jumpstart\tools\ILRepack.exe /target:library /out:C:\projects\Giacomelli-Jumpstart\build\jumpstart.exe /wildcards jumpstart.exe /wildcards *.*
+C:\projects\jumpstart\tools\ILRepack.exe /target:library /out:C:\projects\jumpstart\build\jumpstart.exe /wildcards jumpstart.exe /wildcards *.*
 echo ILRepack finished.
 
-cd C:\projects\Giacomelli-Jumpstart
+cd C:\projects\jumpstart
 
 echo done!
