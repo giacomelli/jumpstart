28efa3a0e6386b7e56b235dd59a3dc729ba6717a
Diego Giacomelli <giacomelli@gmail.com>
2016-09-10 15:44:02 -0300

    AppVeyor config.

--- tools/build-ci.cmd
+++ tools/build-ci.cmd
@@ -1,7 +1,7 @@
 echo Building jumpstart.exe...
 
 mkdir C:\projects\jumpstart\build
-cd C:\projects\jumpstart\src\Giacomelli-Jumpstart\bin\Release
+cd C:\projects\jumpstart\src\Giacomelli.JumpStart\bin\Release
 
 echo Calling ILRepack...
 C:\projects\jumpstart\tools\ILRepack.exe /target:library /out:C:\projects\jumpstart\build\jumpstart.exe /wildcards jumpstart.exe /wildcards *.*
